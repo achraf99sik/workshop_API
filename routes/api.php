fc63e5f0f404dc29d4fbf46249f17a500e9b912e
achraf <achrafsikal@gmail.com>
2025-03-19 10:47:42 +0000

    feat: add phone api resource to routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhoneController;
+
+Route::apiResource('phones', PhoneController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
